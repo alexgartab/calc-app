9b7d4ac1c99e4ce09730ac7ed0cee8bc706be005
Alejandro Garrido <alejandro.garrido@vipera.com>
2021-04-10 11:27:39 +0200

    Calculadora

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// importar el componente
+
+// usar el componente
+ReactDOM.render(<App />, document.getElementById('root'))
